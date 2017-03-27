f83050a10957a4cc74f660b8d39e5a54d07ef3b0
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-03-27 16:53:46 +0100

    added cat

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'shotgun'
+
 set :session_secret, 'super secret'
 
 get '/' do
@@ -16,4 +17,10 @@ end
 
 get '/vivien' do
   "VIVIEN"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
